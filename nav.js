4fc35da9105b80415780ccd780c959756c4487d9
Sergio <sergioacostaportocarrero.alumnado@guadalupe.fundacionloyola.net>
2024-01-08 09:44:58 +0100

    stats added

--- nav.js
+++ nav.js
@@ -2,14 +2,14 @@ window.onload = function() {
     var navItems = document.querySelectorAll('nav ul li a'); // Select anchor tags inside list items
     var maxWidth = 0;
 
-    // Find the widest item
+    // elemento mas ancho
     navItems.forEach(function(item) {
         if (item.offsetWidth > maxWidth) {
             maxWidth = item.offsetWidth;
         }
     });
 
-    // Set all items to the width of the widest item
+    // todos los elementos al mismo ancho
     navItems.forEach(function(item) {
         item.style.width = maxWidth + 'px';
         item.style.display = 'inline-block'; // Ensure the anchor tags behave like block elements
